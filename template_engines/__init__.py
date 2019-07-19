37b118d19822da83d9ced9112da5dd3132c270d7
J-E Castagnede <j.e.castagnede@gmail.com>
2019-07-19 13:49:09 +0200

    use version in file

---
+++ template_engines/__init__.py
@@ -0,0 +1,6 @@
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+__version__ = open(os.path.join(os.path.dirname(here),
+                                'VERSION.md')).read().strip()

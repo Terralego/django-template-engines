903896e0151af83a89e6df3d8080afdba7f4ecb2
J-E Castagnede <j.e.castagnede@gmail.com>
2019-10-10 17:46:35 +0200

    fix image 64

--- template_engines/templatetags/odt_tags.py
+++ template_engines/templatetags/odt_tags.py
@@ -11,7 +11,7 @@ register = template.Library()
 
 
 @register.simple_tag
-def image_loader(image, *args, **kwargs):
+def image_loader(image, i_width=None, i_height=None):
     """
     Replace a tag by an image you specified.
     You must add an entry to the ``context`` var that is a dict with at least a ``content`` key
@@ -23,8 +23,8 @@ def image_loader(image, *args, **kwargs):
             image = {'content': base64.b64decode(image.split(';base64,')[1])}
         else:
             return
-    width = kwargs.get('width', image.get('width'))
-    height = kwargs.get('height', image.get('height'))
+    width = i_width or image.get('width'))
+    height = i_height or image.get('height'))
     content = image.get('content')
 
     width, height = resize(content, width, height)

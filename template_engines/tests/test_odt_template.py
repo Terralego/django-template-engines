a2063300a6f5e8f0ebc0bfe160d44ff8c9eb6ff5
LePetitTim <timothee.de_montety@isen.yncrea.fr>
2019-10-30 17:20:57 +0100

    Add test odt template url

--- template_engines/tests/test_odt_template.py
+++ template_engines/tests/test_odt_template.py
@@ -3,7 +3,7 @@ import os
 from django.test import TestCase, RequestFactory
 
 from template_engines.tests.fake_app.models import Bidon
-from template_engines.tests.fake_app.views import OdtTemplateView, DocxTemplateView
+from template_engines.tests.fake_app.views import OdtTemplateView
 
 from .settings import TEMPLATES_PATH
 
@@ -56,39 +56,48 @@ class TestOdtTemplateView(TestCase):
         self.assertTrue(response.content)
 
 
-class TestDocxTemplateView(TestCase):
+class TestOdtTemplateViewWithHTML(TestCase):
     def setUp(self):
         self.factory = RequestFactory()
-        self.object = Bidon.objects.create(name='Michel')
         self.request = self.factory.get('')
+        OdtTemplateView.template_name = os.path.join(TEMPLATES_PATH, 'html.odt')
 
-    def test_view_works(self):
-        DocxTemplateView.template_name = os.path.join(TEMPLATES_PATH, 'works.docx')
-        response = DocxTemplateView.as_view()(self.request, **{'pk': self.object.pk}).render()
+    def test_br(self):
+        obj = Bidon.objects.create(name='<p><br></p>')
+        response = OdtTemplateView.as_view()(self.request, **{'pk': obj.pk}).render()
         self.assertEqual(response.status_code, 200)
-        self.assertTrue(response.content)
 
-    def test_view_works_with_new_line(self):
-        DocxTemplateView.template_name = os.path.join(TEMPLATES_PATH, 'works.docx')
-        obj = Bidon.objects.create(name='Michel\nPierre')
-        response = DocxTemplateView.as_view()(self.request, **{'pk': obj.pk}).render()
+    def test_a(self):
+        obj = Bidon.objects.create(name='<a href="http://test.com">test.com</a>')
+        response = OdtTemplateView.as_view()(self.request, **{'pk': obj.pk}).render()
         self.assertEqual(response.status_code, 200)
-        self.assertTrue(response.content)
 
-    def test_view_works_with_bold_text(self):
-        DocxTemplateView.template_name = os.path.join(TEMPLATES_PATH, 'works.docx')
-        obj = Bidon.objects.create(name='Michel <b>Pierre</b>')
-        response = DocxTemplateView.as_view()(self.request, **{'pk': obj.pk}).render()
+    def test_ul(self):
+        obj = Bidon.objects.create(name='<ul><li>element 1</li></ul>')
+        response = OdtTemplateView.as_view()(self.request, **{'pk': obj.pk}).render()
         self.assertEqual(response.status_code, 200)
-        self.assertTrue(response.content)
 
-    def test_view_empty_image(self):
-        DocxTemplateView.template_name = os.path.join(TEMPLATES_PATH, 'empty_image.docx')
-        with self.assertRaises(IOError):
-            DocxTemplateView.as_view()(self.request, **{'pk': self.object.pk}).render()
-
-    def test_view_resize(self):
-        DocxTemplateView.template_name = os.path.join(TEMPLATES_PATH, 'resize.docx')
-        response = DocxTemplateView.as_view()(self.request, **{'pk': self.object.pk}).render()
+    def test_ol(self):
+        obj = Bidon.objects.create(name='<ol><li>element 1</li></ol>')
+        response = OdtTemplateView.as_view()(self.request, **{'pk': obj.pk}).render()
         self.assertEqual(response.status_code, 200)
-        self.assertTrue(response.content)
+
+    def test_strong(self):
+        obj = Bidon.objects.create(name='<p><strong>Strong</strong></p>')
+        response = OdtTemplateView.as_view()(self.request, **{'pk': obj.pk}).render()
+        self.assertEqual(response.status_code, 200)
+
+    def test_em(self):
+        obj = Bidon.objects.create(name='<p><em>Strong</em></p>')
+        response = OdtTemplateView.as_view()(self.request, **{'pk': obj.pk}).render()
+        self.assertEqual(response.status_code, 200)
+
+    def test_u(self):
+        obj = Bidon.objects.create(name='<p><u>Strong</u></p>')
+        response = OdtTemplateView.as_view()(self.request, **{'pk': obj.pk}).render()
+        self.assertEqual(response.status_code, 200)
+
+    def test_h(self):
+        obj = Bidon.objects.create(name='<h1>Title 1</h1><h2>Title 2</h2><h3>Title 3</h3>')
+        response = OdtTemplateView.as_view()(self.request, **{'pk': obj.pk}).render()
+        self.assertEqual(response.status_code, 200)

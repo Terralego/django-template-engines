2a3bae40e16c280f42806fe3cd24568100319e10
J-E Castagnede <j.e.castagnede@gmail.com>
2020-10-14 17:35:55 +0200

    fix node name

--- template_engines/templatetags/pdf_tags.py
+++ template_engines/templatetags/pdf_tags.py
@@ -30,7 +30,6 @@ class ImageLoaderURLNode(template.Node):
         extension = get_extension_picture(picture)
         base64_encoded_data = base64.b64encode(picture)
         base64_message = base64_encoded_data.decode('utf-8')
-        raise template.TemplateSyntaxError(base64_message)
         return mark_safe(f"data:image/{extension};base64,{base64_message}")
 
     def get_value_context(self, context):
